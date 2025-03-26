7d62c5bc0695aa0345eef4e1354e006ae6995a66
장연식 <sseregit@knowwheresoft.com>
2025-03-26 17:31:32 +0900

    Spring initializer

---
+++ src/main/java/io/github/sseregit/redishandsonenterprise/RedisHandsOnEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package io.github.sseregit.redishandsonenterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisHandsOnEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisHandsOnEnterpriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sseregit/redishandsonenterprise/RedisHandsOnEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sseregit.redishandsonenterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisHandsOnEnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
